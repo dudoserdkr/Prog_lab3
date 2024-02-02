508fe9da7a46861abb7df80c1b9062b11c44ad24
Lina Yavdoshak <144603177+linyawd@users.noreply.github.com>
2024-02-02 11:54:00 +0200

    func proste is normal now

--- utils.py
+++ utils.py
@@ -14,17 +14,8 @@ def func1(a):
 
     return True
 
-
-print(func1(625))
-#просте число
-n = int(input())
-
 def proste(el):
     for d in range(2, int(el**0.5)+1):
         if el % d == 0:
-            return False
-    return True
-if proste(n) == True:
-    print("Це число просте")
-else:
-    print("Це число не є просте")
+            return "Це число не є просте"
+    return "Це число є просте"
