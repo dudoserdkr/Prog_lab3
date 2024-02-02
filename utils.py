de7789ee5660a017d0072ba24aa47f5ff9384fdc
Lina Yavdoshak <144603177+linyawd@users.noreply.github.com>
2024-02-02 11:31:11 +0200

    adding prog prostechuslo to utils

--- utils.py
+++ utils.py
@@ -16,4 +16,15 @@ def func1(a):
 
 
 print(func1(625))
+#просте число
+n = int(input())
 
+def proste(el):
+    for d in range(2, int(el**0.5)+1):
+        if el % d == 0:
+            return False
+    return True
+if proste(n) == True:
+    print("Це число просте")
+else:
+    print("Це число не є просте")
