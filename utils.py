b908b2655f4ab0ddec3a7a2dd655abe780b54732
dashazashkolna <inkognitodz28@gmail.com>
2024-02-02 11:21:53 +0200

    new

--- utils.py
+++ utils.py
@@ -4,16 +4,8 @@ def factorial(a):
     
     return a*(a-1)
 
-def func1(a):
-    while True:
-        if a % 5 != 0 and a != 1 and a != 0:
-            return False
-        elif a == 1 or a == 0:
-            break
-        a //= 5
 
-    return True
-
-
-print(func1(625))
-
+def nsd(a,b):
+    while b > 0:
+        a, b = b, a % b
+    return a
