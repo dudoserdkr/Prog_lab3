5c9f8eea0f0af1959c0ab3c0c4b91928d3ecfa68
dudoserdkr <ivyur@knu.ua>
2024-02-02 11:14:59 +0200

    h)

--- utils.py
+++ utils.py
@@ -1,6 +1,19 @@
 def factorial(a):
-    if a == 1 or a == 0::
+    if a == 1 or a == 0:
         return 1
     
     return a*(a-1)
-    
+
+def func1(a):
+    while True:
+        if a % 5 != 0 and a != 1 and a != 0:
+            return False
+        elif a == 1 or a == 0:
+            break
+        a //= 5
+
+    return True
+
+
+print(func1(625))
+
